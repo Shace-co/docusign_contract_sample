6b0c5dbfb0aa3f27cb31a9d91aed64d4232c53f4
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-20 12:52:10 +0300

    initial push

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+import htmlBeautifier from "astro-html-beautifier";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [htmlBeautifier()]
+});
